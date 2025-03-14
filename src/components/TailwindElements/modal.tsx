532c7374521d599a125fc290c04a3e1945eddcb3
Chris Williamson <chriswill0629@gmail.com>
2025-03-14 11:47:09 -0400

    Working on parsing xlsx file

--- src/components/TailwindElements/modal.tsx
+++ src/components/TailwindElements/modal.tsx
@@ -1,134 +1,149 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import SelectBox from "./selectBox.tsx";
-import { processFile } from "../../helpers/addByExcelHelpers.tsx";
-import { Employee } from "../../interfaces/employeeInterface.tsx";
+import {processFile} from "../../helpers/addByExcelHelpers.tsx";
+import {Employee} from "../../interfaces/employeeInterface.tsx";
 import TempTitle from "../tabComponents/displayComponents/tempTitle.tsx";
 import TempDisplay from "../tabComponents/displayComponents/tempDisplay.tsx";
-import { addEmployeeHelper } from "../../helpers/addEmployeeHelper.tsx";
-import { db } from "../../helpers/db.ts";
+import {addEmployeeHelper} from "../../helpers/addEmployeeHelper.tsx";
+import {db} from "../../helpers/db.ts";
 
 interface Props {
-  isOpen: boolean;
-  onClose: () => void;
-  teamLeads: string[];
-  file: any;
-  setSaved: () => (value: ((prevState: boolean) => boolean) | boolean) => void;
+    isOpen: boolean;
+    onClose: () => void;
+    teamLeads: string[];
+    file: any;
+    setSaved: () => void;
+    saveMessage: string;
 }
 
 export default function Modal({
-  isOpen,
-  onClose,
-  teamLeads,
-  file,
-  setSaved,
-}: Props) {
-  const [warning, setWarning] = useState(""); // 'border-3 border-red-600'
-  const [TL, setTL] = useState("");
-  const [employees, setEmployees] = useState<Employee[]>([]);
-  const [message, setMessage] = useState(""); // "Employees saved successfully!"
+                                  isOpen,
+                                  onClose,
+                                  teamLeads,
+                                  file,
+                                  setSaved,
+                                  saveMessage,
+                              }: Props) {
+    const [warning, setWarning] = useState(""); // 'border-3 border-red-600'
+    const [selectedTL, setSelectedTL] = useState("");
+    const [employees, setEmployees] = useState<Employee[]>([]);
+    const [message, setMessage] = useState(""); // "Employees saved successfully!"
 
-  const handleTLSelect = (key, val) => {
-    setTL(val);
-  };
-  const handleSetEmployees = (val: Employee[]) => setEmployees(val);
+    const handleTLSelect = (key: string, val: string) => {
+        // key never used due to reusing selectbox component
+        setSelectedTL(val);
+    };
+    const handleSetEmployees = (val: Employee[]) => setEmployees(val);
 
-  const handleClick = (e: any) => {
-    processFile(e, file, TL, handleSetEmployees);
-  };
+    const handleClick = (e: any) => {
+        processFile(e, file, selectedTL, handleSetEmployees);
+    };
 
-  const handleSubmit = () => {
-    db.employees.clear();
+    const handleSubmit = () => {
+        if (saveMessage === '') {
 
-    employees.map((emp) => {
-      addEmployeeHelper(emp);
-    });
-    setSaved(true);
-    setMessage("Employees saved successfully!");
-  };
-
-  const renderEmps = () => {
-    let count = 0;
-    return (
-      <>
-        <TempTitle />
-        {employees.map((emp) => {
-          return (
-            <TempDisplay
-              key={count++}
-              shiftStart={emp.shiftStart}
-              shiftEnd={emp.shiftEnd}
-              daysWorked={emp.daysWorked}
-              firstName={emp.firstName}
-              lastName={emp.lastName}
-              email={emp.email}
-              EEID={emp.EEID}
-              meetings={emp.meetings}
-              meetingsDay={emp.meetingsDay}
-              warnings={emp.warnings}
-            />
-          );
-        })}
-        <div className="flex justify-center">
-          <div
-            className="w-[75%] bg-red-500 rounded-md p-2 m-2 text-center"
-            onClick={handleSubmit}
-          >
-            SAVE EMPLOYYEES
-          </div>
-        </div>
-      </>
-    );
-  };
-
-  return (
-    <div
-      className={`fixed inset-0 z-50 flex items-center justify-center bg-black/75 ${
-        isOpen ? "block " : "hidden "
-      }`}
-    >
-      <div
-        className={
-          "bg-white text-black p-1 pl-2 pr-2 rounded shadow-lg w-[95%] max-h-[95%] " +
-          warning
         }
-      >
-        <div className="flex justify-between">
-          <div className="w-90 prose">
-            <h2>Add Employees From Excel</h2>
-          </div>
-          <div
-            className=" bg-black text-white p-2 rounded-md cursor-pointer"
-            onClick={onClose}
-          >
-            Close
-          </div>
-        </div>
+        db.employees.clear();
+        employees.map((emp) => {
+            addEmployeeHelper(emp);
+        });
+        setSaved(true);
+        setMessage("Employees saved successfully!");
+    };
 
-        <div className="flex justify-start pb-3">
-          <label className={"pt-2"}>Please Select A Team Lead: </label>
-          <SelectBox
-            keyValue={"TeamLeads"}
-            arr={teamLeads}
-            error={false}
-            value={TL}
-            setter={handleTLSelect}
-          />
-        </div>
+    const renderEmps = () => {
+        let count = 0;
+        return (
+            <>
+                <TempTitle/>
+                {employees.map((emp) => {
+                    return (
+                        <TempDisplay
+                            key={count++}
+                            shiftStart={emp.shiftStart}
+                            shiftEnd={emp.shiftEnd}
+                            daysWorked={emp.daysWorked}
+                            firstName={emp.firstName}
+                            lastName={emp.lastName}
+                            email={emp.email}
+                            EEID={emp.EEID}
+                            meetings={emp.meetings}
+                            meetingsDay={emp.meetingsDay}
+                            warnings={emp.warnings}
+                        />
+                    );
+                })}
+                <div className="flex justify-center">
+                    {saveMessage !== '' &&
+                        <div className="border-2 border-red-800 bg-red-200 text-black justify-center">
+                            <p>Please confirm these changes by clicking save again.</p>
+                            <ul>
+                                <li>All employees will be removed</li>
+                                <li>All employees NOTES will be removed</li>
+                                <li>If you need the notes then now is the time to copy the information.</li>
+                            </ul>
+                        </div>
+                    }
+                    <div
+                        className="w-[75%] bg-red-500 rounded-md p-2 m-2 text-center"
+                        onClick={handleSubmit}
+                    >
+                        SAVE EMPLOYYEES
+                    </div>
+                </div>
+            </>
+        );
+    };
+
+    return (
         <div
-          className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-1 text-center w-[220px] h-full ml-5 mr-2"
-          onClick={(e) => handleClick(e)}
+            className={`fixed inset-0 z-50 flex items-center justify-center bg-black/75 ${
+                isOpen ? "block " : "hidden "
+            }`}
         >
-          Generate
-        </div>
-        <div className="flex justify-center">
-          {message !== "" && (
-            <div className="p-2 text-center bg-yellow-200 rounded-md w-[50%]">
-              {message}
+            <div
+                className={
+                    "bg-white text-black p-1 pl-2 pr-2 rounded shadow-lg w-[95%] max-h-[95%] " +
+                    warning
+                }
+            >
+                <div className="flex justify-between">
+                    <div className="w-90 prose">
+                        <h2>Add Employees From Excel</h2>
+                    </div>
+                    <div
+                        className=" bg-black text-white p-2 rounded-md cursor-pointer"
+                        onClick={onClose}
+                    >
+                        Close
+                    </div>
+                </div>
+
+                <div className="flex justify-start pb-3">
+                    <label className={"pt-2"}>Please Select A Team Lead: </label>
+                    <SelectBox
+                        keyValue={"TeamLeads"}
+                        arr={teamLeads}
+                        error={false}
+                        value={selectedTL}
+                        setter={handleTLSelect}
+                    />
+                </div>
+                <div
+                    className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-1 text-center w-[220px] h-full ml-5 mr-2"
+                    onClick={(e) => handleClick(e)}
+                >
+                    Generate
+                </div>
+                <div className="flex justify-center">
+                    {message !== "" && (
+                        <div className="p-2 text-center bg-yellow-200 rounded-md w-[50%]">
+                            {message}
+                        </div>
+                    )}
+                </div>
+                <div className="pt-4">{employees.length !== 0 && renderEmps()}</div>
             </div>
-          )}
         </div>
-        <div className="pt-4">{employees.length !== 0 && renderEmps()}</div>
-      </div>
-    </div>
-  );
+    );
 }
--- src/components/tabComponents/TabTwo-AddByExcel.tsx
+++ src/components/tabComponents/TabTwo-AddByExcel.tsx
@@ -1,76 +1,84 @@
-import { useState } from "react";
+import {useState} from "react";
 import Modal from "../TailwindElements/modal.tsx";
-import { processTL } from "../../helpers/addByExcelHelpers.tsx";
+import {processTL} from "../../helpers/addByExcelHelpers.tsx";
 
 interface Props {
-  show: string;
-  tabShow: string;
-  tabHide: string;
+    show: string;
+    tabShow: string;
+    tabHide: string;
 }
 
 // TODO: `Need to add updating by excel parsing functionality`
-export default function TabTwoAddByExcel({ show, tabShow, tabHide }: Props) {
-  const [saved, setSaved] = useState(false);
-  const [file, setFile] = useState<any>();
-  const [teamLeads, setTeamLeads] = useState<any>([]);
+export default function TabTwoAddByExcel({show, tabShow, tabHide}: Props) {
+    const [saved, setSaved] = useState(false);
+    const [saveMessage, setSaveMessage] = useState('');
+    const [file, setFile] = useState<any>();
+    const [teamLeads, setTeamLeads] = useState<any>([]);
 
-  const handleChange = (e: any) => {
-    const tLeads = e.target.files[0];
-    processTL(e, tLeads, setTeamLeads);
-    setFile(e.target.files[0]);
-  };
+    const [isOpen, setIsOpen] = useState(false);
 
-  const [isOpen, setIsOpen] = useState(false);
-  const openModal = () => {
-    setIsOpen(true);
-  };
-  const closeModal = () => {
-    setIsOpen(false);
-    if (saved) {
-      window.location.reload();
-    }
-  };
+    // Handle the selection of file and store it in state for use
+    const handleChange = (e: any) => {
+        const tLeads = e.target.files[0];
+        processTL(e, tLeads, setTeamLeads);
+        setFile(e.target.files[0]);
+    };
 
-  return (
-    <div
-      className={show === "add-from-excel" ? tabShow : tabHide}
-      id="add-team-member"
-      role="tabpanel"
-      aria-labelledby="add-team-member-tab"
-    >
-      <h3 className="mb-3 text-white">Add From Excel</h3>
-      <div className="flex ">
-        <label
-          className="text-sm text-white font-medium text-gray-900 bg-gray-600 rounded-l-md pt-1 pl-2 w-[120px]"
-          htmlFor="file_input"
+    // Opens the modal to generate team
+    const openModal = () => {
+        setIsOpen(true);
+    };
+    // Closes the modal to generate team
+    const closeModal = () => {
+        setIsOpen(false);
+        if (saved) {
+            window.location.reload();
+        }
+    };
+    // SaveSetter to determine if browser requires refresh to update data
+    const saveSetter = (val: boolean) => setSaved(val)
+
+    return (
+        <div
+            className={show === "add-from-excel" ? tabShow : tabHide}
+            id="add-team-member"
+            role="tabpanel"
+            aria-labelledby="add-team-member-tab"
         >
-          Upload file
-        </label>
-        <input
-          className="w-75 text-sm text-gray-900 border border-gray-300 rounded-r-md cursor-pointer bg-gray-50 focus:outline-none "
-          id="file_input"
-          accept=".xlsx"
-          type="file"
-          onChange={handleChange}
-        />
-        <button
-          // type="submit"
-          onClick={openModal}
-          className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-1 text-center w-[220px] h-full ml-5 mr-2"
-        >
-          Upload File
-        </button>
-        <Modal
-          isOpen={isOpen}
-          onClose={closeModal}
-          teamLeads={teamLeads}
-          file={file}
-          setSaved={setSaved}
-        />
-      </div>
-      <div className="ml-[120px] pt-1 text-xs text-gray-400">
-        *Click into white space to select file
-      </div>
-    </div>
-  );
+            <h3 className="mb-3 text-white">Add From Excel</h3>
+            <div className="flex ">
+                <label
+                    className="text-sm text-white font-medium text-gray-900 bg-gray-600 rounded-l-md pt-1 pl-2 w-[120px]"
+                    htmlFor="file_input"
+                >
+                    Upload file
+                </label>
+                <input
+                    className="w-75 text-sm text-gray-900 border border-gray-300 rounded-r-md cursor-pointer bg-gray-50 focus:outline-none "
+                    id="file_input"
+                    accept=".xlsx"
+                    type="file"
+                    onChange={handleChange}
+                />
+                <button
+                    // type="submit"
+                    onClick={openModal}
+                    className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-1 text-center w-[220px] h-full ml-5 mr-2"
+                >
+                    Upload File
+                </button>
+                <Modal
+                    isOpen={isOpen}
+                    onClose={closeModal}
+                    teamLeads={teamLeads}
+                    file={file}
+                    setSaved={saveSetter}
+                    saveMessage={saveMessage}
+                />
+            </div>
+            <div className="ml-[120px] pt-1 text-xs text-gray-400">
+                *Click into white space to select file
+            </div>
+        </div>
+    );
 }
