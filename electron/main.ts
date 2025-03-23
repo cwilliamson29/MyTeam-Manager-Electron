18efdefa6c10d0e4d722985670156c093796ff24
Chris Williamson <chriswill0629@gmail.com>
2025-03-22 22:41:17 -0400

    fixing some css issues and adding custom-electron-titlebar

--- electron/main.ts
+++ electron/main.ts
@@ -2,14 +2,11 @@ import { app, BrowserWindow } from "electron";
 //import { createRequire } from "node:module";
 import { fileURLToPath } from "node:url";
 import path from "node:path";
-import {
-    attachTitlebarToWindow,
-    setupTitlebar,
-} from "custom-electron-titlebar/main";
+import { attachTitlebarToWindow, setupTitlebar } from "custom-electron-titlebar/main";
 import contextMenu from "electron-context-menu";
 
 contextMenu({
-    showSaveImageAs: true,
+	showSaveImageAs: true,
 });
 
 setupTitlebar();
@@ -32,60 +29,56 @@ export const VITE_DEV_SERVER_URL = process.env["VITE_DEV_SERVER_URL"];
 export const MAIN_DIST = path.join(process.env.APP_ROOT, "dist-electron");
 export const RENDERER_DIST = path.join(process.env.APP_ROOT, "dist");
 
-process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL
-    ? path.join(process.env.APP_ROOT, "public")
-    : RENDERER_DIST;
+process.env.VITE_PUBLIC = VITE_DEV_SERVER_URL ? path.join(process.env.APP_ROOT, "public") : RENDERER_DIST;
 
 let win: BrowserWindow | null;
 
 function createWindow() {
-    win = new BrowserWindow({
-        icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-            preload: path.join(__dirname, "preload.mjs"),
-        },
-        width: 1075,
-        height: 900,
-        frame: false,
-        titleBarStyle: "hidden",
-        titleBarOverlay: true,
-    });
+	win = new BrowserWindow({
+		icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+			preload: path.join(__dirname, "preload.mjs"),
+		},
+		width: 1075,
+		height: 900,
+		frame: false,
+		transparent: true,
+		titleBarStyle: "hidden",
+		titleBarOverlay: true,
+	});
 
-    // Test active push message to Renderer-process.
-    win.webContents.on("did-finish-load", () => {
-        win?.webContents.send(
-            "main-process-message",
-            new Date().toLocaleString(),
-        );
-    });
+	// Test active push message to Renderer-process.
+	win.webContents.on("did-finish-load", () => {
+		win?.webContents.send("main-process-message", new Date().toLocaleString());
+	});
 
-    if (VITE_DEV_SERVER_URL) {
-        win.loadURL(VITE_DEV_SERVER_URL);
-    } else {
-        // win.loadFile('dist/index.html')
-        win.loadFile(path.join(RENDERER_DIST, "index.html"));
-    }
-    attachTitlebarToWindow(win);
+	if (VITE_DEV_SERVER_URL) {
+		win.loadURL(VITE_DEV_SERVER_URL);
+	} else {
+		// win.loadFile('dist/index.html')
+		win.loadFile(path.join(RENDERER_DIST, "index.html"));
+	}
+	attachTitlebarToWindow(win);
 }
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-        win = null;
-    }
+	if (process.platform !== "darwin") {
+		app.quit();
+		win = null;
+	}
 });
 
 app.on("activate", () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
 });
 
 app.whenReady().then(createWindow);
