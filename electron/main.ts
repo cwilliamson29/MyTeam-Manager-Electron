5a74c7e1053c8046fd6c593b333d0370c2d05468
Chris Williamson <chriswill0629@gmail.com>
2025-03-28 23:01:47 -0400

    Working with updater and aded desc to package.json

--- electron/main.ts
+++ electron/main.ts
@@ -4,10 +4,10 @@ import path from "node:path";
 import contextMenu from "electron-context-menu";
 import icon from "../src/assets/react.svg";
 import { autoUpdater } from "electron-updater";
-import { log } from "electron-log/node";
+import { log } from "electron-log";
 
-log.transports.file.level = "debug";
-autoUpdater.logger = log;
+// log.transports.file.level = "debug";
+// autoUpdater.logger = log();
 autoUpdater.autoDownload = false;
 
 contextMenu({
@@ -78,56 +78,61 @@ app.on("activate", () => {
 app.whenReady().then(() => {
 	createWindow();
 	autoUpdater.checkForUpdates();
-	log(autoUpdater);
+
+	//log(autoUpdater.checkForUpdates());
 
 	autoUpdater.on("checking-for-update", () => {
+		//dialog.
 		dialog.showMessageBox({
 			type: "info",
 			title: "Checking for Updates",
 			message: "Checking for updates...",
 		});
 	});
-
+	log("before update-available");
 	autoUpdater.on("update-available", () => {
-		dialog.showMessageBox(
-			{
-				// @ts-ignore
-				type: "info",
+		//log("inside update-available, but befor dia");
+		dialog
+			.showMessageBox({
+				type: "question",
 				title: "Update Available",
 				message: "A new version of the app is available. Do you want to update now?",
 				buttons: ["Update", "No"],
-			},
-			(index: any) => {
-				if (index === 0) {
+			})
+			.then((response) => {
+				log(response.response + ":is the index");
+				//log(":is the index");
+				if (response.response === 0) {
+					log("inside if for download dialog");
+					//log(autoUpdater.downloadUpdate())
 					autoUpdater.downloadUpdate();
 				}
-			},
-		);
+			});
 	});
 
 	autoUpdater.on("download-progress", (progressObj) => {
 		let log_message = "Download speed: " + progressObj.bytesPerSecond;
 		log_message = log_message + " - Downloaded " + progressObj.percent + "%";
 		log_message = log_message + " (" + progressObj.transferred + "/" + progressObj.total + ")";
-		console.log(log_message);
+		log(log_message);
 	});
 
 	// @ts-ignore
 	autoUpdater.on("update-downloaded", (info) => {
-		dialog.showMessageBox(
-			{
-				// @ts-ignore
+		log("inside updater for downloaded");
+		dialog
+			.showMessageBox({
 				type: "info",
 				title: "Update Downloaded",
 				message: "The new version has been downloaded. It will be installed on restart. Do you want to restart now?",
 				buttons: ["Restart", "Later"],
-			},
-			(index: any) => {
-				if (index === 0) {
-					autoUpdater.quitAndInstall();
-				}
-			},
-		);
+			})
+			.then((response) => {
+				log(response + " :update downloaded");
+				// if (index === 0) {
+				// 	autoUpdater.quitAndInstall();
+				// }
+			});
 	});
 });
 
